2932b2b15205c06f041a8106dcfc7be06b06ffc9
ambiso <ambisotopy@gmail.com>
2023-03-18 14:32:45 +0100

    With timestamps

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
+use std::collections::HashSet;
 use std::error::Error;
 use std::process::Stdio;
 use tokio::io::AsyncWriteExt;
@@ -70,6 +71,7 @@ async fn call_chatgpt_api(api_key: &str, prompt: &str) -> Result<String, Box<dyn
         .await?;
 
     let response_body: Root = response.json().await?;
+    //dbg!(response_body); Ok("".to_string())
     let generated_text = response_body.choices[0].message.content.clone();
 
     Ok(generated_text)
@@ -99,22 +101,33 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let output = re.replace_all(&s, "");
 
-    let re2 = Regex::new(r"^\d{2}:\d{2}:\d{2}\.\d{3} --> \d{2}:\d{2}:\d{2}\.\d{3}")?;
-    let mut lines: Vec<_> = output
-        .lines()
-        .filter(|line| !re2.is_match(line))
-        .map(|x| x.trim())
-        .filter(|x| x.len() > 0)
-        .collect();
-    lines.dedup();
+    let re2 = Regex::new(r"^\d{2}:(\d{2}:\d{2})\.\d{3} --> \d{2}:\d{2}:\d{2}\.\d{3}")?;
+    let mut lines_map = HashSet::new();
+    let mut formatted_lines = Vec::new();
+    let mut cur_timestamp = None;
+    let every_n_lines = 10;
+    for line in output.lines() {
+        let line = line.trim();
+        if let Some(timestamp) = re2.captures(line).and_then(|x| x.get(1)) {
+            cur_timestamp = Some(timestamp.as_str());
+        } else if line.len() > 0 && cur_timestamp.is_some() && !lines_map.contains(line) {
+            if formatted_lines.len() % every_n_lines == 0 {
+                formatted_lines.push(format!("{}: {}", cur_timestamp.unwrap(), line));
+            } else {
+                formatted_lines.push(format!("{}", line));
+            }
+            lines_map.insert(line);
+        }
+    }
 
-    let subs = lines.join("\n");
+    let subs = formatted_lines.join("\n");
+    //println!("{}", subs);
 
     let api_key = String::from_utf8(tokio::fs::read("api_key.txt").await?)?;
 
     stdout.write_all(b"Asking for a summary...\n\n").await?;
     stdout.flush().await?;
-    let input = format!("Can you summarize the video whose subtitles are below? After the quick summary, provide a more detailed overview of the topics discussed in the video, and briefly touch on them. \n\nSUBTITLES START:\n{}\n\nSUBTITLES END\n", subs);
+    let input = format!("Can you summarize the video whose subtitles are below? After the quick summary, provide a more detailed overview of the topics discussed in the video, and briefly touch on them. Then provide a very detailed listing of each subject discussed and the corresponding timestamps where each subject can be found.\n\nSUBTITLES START:\n{}\n\nSUBTITLES END\n", subs);
     let response = call_chatgpt_api(&api_key, &input).await?;
     stdout.write_all(response.as_bytes()).await?;
     stdout.write_all(b"\n").await?;
